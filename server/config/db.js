d561c8ee1b4789564e7d09bfd75d059c05bfb68a
GIRISHGHUGARE <girishghugare19@gmail.com>
2025-01-04 10:23:35 +0530

    Backend Update!

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,16 @@
+// PACKAGES
 const mongoose = require("mongoose");
 
+// CREATE ASYNC FUNCTION TO USE AWAIT WHILE CONNECTING TO MONGODB 
 const connectDB = async () => {
     try {
-        const connection = new mongoose.connect(process.env.MONGO_URL)
-    } catch (error) {
-
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDB connected".bgCyan.white);
+    } catch (err) {
+        console.error("MongoDB connection error: ".bgRed.white, err);
+        process.exit(1);  // EXIT ON FAILURE
     }
-}
+};
 
-module.exports = connectDB
+// EXPORT FUNCTION
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,66 @@
+// PACKAGES
 const express = require("express");
 const morgan = require("morgan");
-const dotenv = require("dotenv");
 const cors = require("cors");
 const colors = require("colors");
-const { default: connectDB } = require("./config/db");
+const dotenv = require("dotenv");
+const cookieParser = require('cookie-parser');
+const path = require('path')
 
+// FILES
+const connectDB = require('./config/db');
+const userRoutes = require("./routes/userRoutes")
+
+// DOTENV
 dotenv.config();
 
+// MONGODB CONNECTION
+connectDB();
+
+// REST OBJECT
 const app = express();
 
-const PORT = process.env.PORT;
+// const __dirname = path.resolve();
 
-app.use(morgan());
-app.use(cors(
-    origin = ""
-))
+// MIDDLEWARES
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+};
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors(corsOptions));
+}
+app.use(cookieParser()); // TO PARSE COOKIE
+app.use(express.json());
+app.use(morgan("dev")); // TO HAVE CONSOLE LOGS SUCCESS AND ERROR
 
 
-app.use("", () => {
-    console.log("Welcome to Server!!");
-})
+// MAIN ROUTE
+app.get("", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to Varkari Sampraday Server!!"
+    });
+});
 
-app.listen(PORT, (req, res) => {
-    console.log(`Server running at port ${PORT}`.bgCyan.white);
-    connectDB()
+// OTHER ROUTES
+//app.use("/api/v1/auth", userRoutes)
+
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+// CONNECTION OF FRONTEND AND BACKEND FOR HOSTING
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "client/dist")));
+
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+//     });
+// }
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Server Running ${PORT}`.bgGreen.white);
 })
